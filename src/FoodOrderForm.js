09eba41dc7cdd334cfd40ad15b664402d32fff57
Ferdinand Lukito <devferdinandlukito@gmail.com>
2023-09-27 12:31:12 -0400

    Prevented form default action and added submit handler for form

--- src/FoodOrderForm.js
+++ src/FoodOrderForm.js
@@ -6,19 +6,48 @@ function FoodOrderForm() {
     const [address, setAddress] = useState("");
     const [order, setOrder] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Order Successful!\n\nYour order was ${order}.\n\nPlease show your confirmation number for pickup.`);
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="name">Name:</label>
-            <input id="name" value={name} onChange={(e) => setName(e.target.value)}></input>
+            <input 
+                id="name" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)} 
+                name="name" 
+                type="text" 
+            />
 
             <label htmlFor="phone">Phone:</label>
-            <input id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}></input>
+            <input 
+                id="phone" 
+                value={phone} 
+                onChange={(e) => setPhone(e.target.value)} 
+                name="phone"
+                type="tel"
+            />
 
             <label htmlFor="address">Address:</label>
-            <input id="address" value={address} onChange={(e) => setAddress(e.target.value)}></input>
+            <input 
+                id="address" 
+                value={address} 
+                onChange={(e) => setAddress(e.target.value)} 
+                name="address"
+                type="text"   
+            />
 
             <label htmlFor="order">Order:</label>
-            <input id="order" value={order} onChange={(e) => setOrder(e.target.value)}></input>
+            <input 
+                id="order" 
+                value={order} 
+                onChange={(e) => setOrder(e.target.value)}
+                name="order"
+                type="text"
+            />
 
             <button type="submit">Submit Order</button>
         </form>
