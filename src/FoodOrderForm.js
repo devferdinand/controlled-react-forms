9d751f05bbd1f1680ec4160e6ce7306135428f51
Ferdinand Lukito <devferdinandlukito@gmail.com>
2023-09-27 12:07:08 -0400

    Added labels and inputs to form and handed control of form to React through state variables

--- src/FoodOrderForm.js
+++ src/FoodOrderForm.js
@@ -1,7 +1,28 @@
 import React, { useState } from "react";
 
 function FoodOrderForm() {
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    const [order, setOrder] = useState("");
 
+    return (
+        <form>
+            <label htmlFor="name">Name:</label>
+            <input id="name" value={name}></input>
+
+            <label htmlFor="phone">Phone:</label>
+            <input id="phone" value={phone}></input>
+
+            <label htmlFor="address">Address:</label>
+            <input id="address" value={address}></input>
+
+            <label htmlFor="order">Order:</label>
+            <input id="order" value={order}></input>
+
+            <button type="submit">Submit Order</button>
+        </form>
+    );
 }
 
 export default FoodOrderForm;
